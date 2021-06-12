c28135ae1078c191e5ad3fecfd9bb2355ff106a8
miguelUn <55412583+miguelUn@users.noreply.github.com>
2021-06-12 09:40:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<h3>Hello CI/CD!</h3>"
+    return html
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
